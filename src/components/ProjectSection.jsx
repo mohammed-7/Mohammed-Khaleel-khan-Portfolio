f6d20f3911d74497d1815b0fc9b720f65ca831ea
mohammed <khuzaif254@gmail.com>
2025-09-26 14:25:40 +0530

    finished skillsection-started projectsec

---
+++ src/components/ProjectSection.jsx
@@ -0,0 +1,5 @@
+const projects = [{}];
+
+export const ProjectSection = () => {
+  return <section></section>;
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { StarBackground } from '@/components/StarBackground';
 import { HeroSection } from '../components/HeroSection';
 import { AboutSection } from '../components/AboutSection';
 import { SkillsSection } from '../components/SkillsSection';
+import { ProjectSection } from '../components/ProjectSection';
 
 export const Home = () => {
   return (
@@ -22,6 +23,7 @@ export const Home = () => {
         <HeroSection />
         <AboutSection />
         <SkillsSection />
+        <ProjectSection />
       </main>
 
       {/* Footer */}
