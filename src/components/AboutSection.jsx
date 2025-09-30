97fddde61e62ce0fc5a47daddfda34a26cafaca0
mohammed <khuzaif254@gmail.com>
2025-09-30 10:30:45 +0530

    new changes added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href=""
+                href="/public/Mohammed_khaleel_khan.pdf"
                 className="px-6 py-2 rounded-full border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
